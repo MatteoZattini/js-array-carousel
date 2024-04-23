8c59f5faf8751c3a1aaccded55c34db68ffb5bd0
MatteoZattini <matteozattini@hotmail.com>
2024-04-24 01:31:38 +0200

    found the solution to the first script but i have no idea how it works

--- main.js
+++ main.js
@@ -1,77 +1,79 @@
-// let slides = document.getElementsByClassName("slide");
+let slides = document.getElementsByClassName("slide");
 
-// let nSlides = slides.length
+let nSlides = slides.length
 
-// let slideSelected = 0
+let slideSelected = 0
 
-// document.getElementById("btnNext").addEventListener('click', function(){
-//     slides[slideSelected].classList.remove("active")
-//     if( slideSelected < nSlides - 1 ){
-//         slideSelected++;
-//     } else {
-// 		slideSelected = 0;
-// 	}
-// 	slides[slideSelected].classList.add("active");
-// 	console.log("Selezione -> aggiungi", slideSelected);
-// })
+document.getElementById("btnNext").addEventListener('click', function(){
+    slides[slideSelected].classList.remove("active")
+    if( slideSelected < nSlides - 1 ){
+        slideSelected++;
+    } else {
+		slideSelected = 0;
+	}
+	slides[slideSelected].classList.add("active");
+	console.log("Selezione -> aggiungi", slideSelected);
+})
 
-// document.getElementById("btnPrev").addEventListener('click', function(){
-//     slides[slideSelected].classList.remove("active")
-//     if( slideSelected < nSlides ){
-//         slideSelected--;
-//     } else {
-// 		slideSelected = 4;
-// 	}
-//     slides[slideSelected].classList.add("active")
-//     console.log("Selezione -> aggiungi", slideSelected);
+document.getElementById("btnPrev").addEventListener('click', function(){
+    slides[slideSelected].classList.remove("active")
 
-// })
+    if( slideSelected - 0 + nSlides % nSlides ){
+        slideSelected--;
+		console.log(slideSelected)
+    } else {
+		slideSelected = 4;
+	}
+	slides[slideSelected].classList.add("active");
+    console.log("Selezione -> aggiungi", slideSelected);
+
+})
 
 // ciclo for
 
 
-let immagineAttualmenteAttiva;
-let prossimaSlide;
+// let immagineAttualmenteAttiva;
+// let prossimaSlide;
 
 
-let slides = document.getElementsByClassName("slide");
+// let slides = document.getElementsByClassName("slide");
 
-document.getElementById("btnNext").addEventListener("click", function () {	
-	for (let i = 0; i < slides.length; i++) {
-		const slide = slides[i];		
+// document.getElementById("btnNext").addEventListener("click", function () {	
+// 	for (let i = 0; i < slides.length; i++) {
+// 		const slide = slides[i];		
 		
-		if (slide.classList.contains("active")) {			
-			slide.classList.remove("active");		
-			immagineAttualmenteAttiva = i;			
-		}
-	}
+// 		if (slide.classList.contains("active")) {			
+// 			slide.classList.remove("active");		
+// 			immagineAttualmenteAttiva = i;			
+// 		}
+// 	}
 	
-	prossimaSlide = immagineAttualmenteAttiva + 1;
+// 	prossimaSlide = immagineAttualmenteAttiva + 1;
 	
-	if (prossimaSlide >= slides.length) {
-		prossimaSlide = 0;
-	}
+// 	if (prossimaSlide >= slides.length) {
+// 		prossimaSlide = 0;
+// 	}
 	
-	slides[prossimaSlide].classList.add("active");
-	console.log(`Sposto la selezione da ${immagineAttualmenteAttiva} a ${prossimaSlide}`);
-});
+// 	slides[prossimaSlide].classList.add("active");
+// 	console.log(`Sposto la selezione da ${immagineAttualmenteAttiva} a ${prossimaSlide}`);
+// });
 
-document.getElementById("btnPrev").addEventListener("click", function () {
-    for (let i = 0; i < slides.length; i++) {
-        const slide = slides[i];
+// document.getElementById("btnPrev").addEventListener("click", function () {
+//     for (let i = 0; i < slides.length; i++) {
+//         const slide = slides[i];
 
-        if (slide.classList.contains("active")) {			
-			slide.classList.remove("active");		
-			immagineAttualmenteAttiva = i;			
-		}
-    }
+//         if (slide.classList.contains("active")) {			
+// 			slide.classList.remove("active");		
+// 			immagineAttualmenteAttiva = i;			
+// 		}
+//     }
 
-    prossimaSlide = immagineAttualmenteAttiva - 1;
+//     prossimaSlide = immagineAttualmenteAttiva - 1;
 
-    if (prossimaSlide < 0) {
-        prossimaSlide = 0 + 4;
-    }
+//     if (prossimaSlide < 0) {
+//         prossimaSlide = 0 + 4;
+//     }
 
-    slides[prossimaSlide].classList.add("active");
-	console.log(`Sposto la selezione da ${immagineAttualmenteAttiva} a ${prossimaSlide}`);
-})
+//     slides[prossimaSlide].classList.add("active");
+// 	console.log(`Sposto la selezione da ${immagineAttualmenteAttiva} a ${prossimaSlide}`);
+// })
